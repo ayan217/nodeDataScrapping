b66cb08450f991cb8a47d850b19d401558e8a821
Ayan Karmakar <ayan.karmakar@rnf.tech>
2025-04-03 18:51:20 +0530

    base pro fetch done

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const Scrap = require('./controllers/Scrap.js');
 const scrapper = new Scrap();
 
 (async () => {
-    const product = await scrapper.getSingleProduct(28018);
+    const product = await scrapper.getSingleProduct(28022);
     const linkToScrap = product.link;
+    console.log(linkToScrap);
+    const singleProductData = await scrapper.scrapSingleProduct(linkToScrap);
+    console.log(singleProductData);
 })();
 
-
+// console.log(scrapper.getAllBaseLinks());
--- controllers/Scrap.js
+++ controllers/Scrap.js
@@ -5,8 +5,34 @@ class Scrap {
     async sync() {
         await syncModels();
     }
+    async scrapSingleProduct(url) {
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: '/usr/bin/google-chrome',
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+
+        await page.goto(url, { waitUntil: 'networkidle2' });
+
+        const data = await page.evaluate(() => {
+            const productName = document.querySelector('h1[data-aid="PRODUCT_NAME_RENDERED"]')?.innerText;
+            const productPrice = document.querySelector('div[data-aid="PRODUCT_PRICE_RENDERED"]')?.innerText;
+            const productDes = document.querySelector('div[data-aid="PRODUCT_DESCRIPTION_RENDERED"]')?.innerHTML;
+            const productImage = document.querySelector('img[data-ux="Image"]')?.src;
+            const selectBox = document.querySelector('select[data-ux="InputSelectElement"]');
+            const options = selectBox ? Array.from(selectBox.options).map(option => option.innerText).filter(option => option !== "-- Select --") : [];
+            return { productName, productPrice, productImage, productDes, options };
+        });
+        await browser.close();
+        return data;
+    }
     async scrapBaseLinks(url) {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: '/usr/bin/google-chrome',
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
 
         await page.goto(url, { waitUntil: 'networkidle2' });
@@ -64,7 +90,7 @@ class Scrap {
     async getSingleProduct(id) {
         const row = await AllLinks.findByPk(id);
         // console.log(JSON.stringify(row, null, 2));
-       return row;
+        return row;
     }
 }
 
